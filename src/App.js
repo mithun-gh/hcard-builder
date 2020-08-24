1d2021256e671b261d28e9468a85f240a7791298
Mithun <mithunrathinasamy@live.com>
2020-08-24 20:24:14 +1000

    feat: add style libraries and Button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+
+import Button from "./ui-components/Button";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button primary>Create hCard</Button>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui-components/Button.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  height: 48px;
+  padding: 12px 28px;
+  font-size: 20px;
+  outline: none;
+  cursor: pointer;
+  border: none;
+  border-radius: 4px;
+  border-bottom: 2px solid #00000080;
+  color: #fff;
+  background-color: ${(props) => (props.primary ? "#3498db" : "#627b8b")};
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
+
+export default Button;
